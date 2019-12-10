004a57963dd8a892797a8bc8a77d83d7ad7b242e
Gontar87 <34239520+Gontar87@users.noreply.github.com>
2019-12-10 22:20:05 +0100

    Karty

--- app/views.py
+++ app/views.py
@@ -19,22 +19,29 @@ def upload():
 @app.route('/formularz', methods=["GET", "POST"])
 def formularz():
     if request.method == "POST":
-        r = requests.get('http://chceptasznika.herokuapp.com/spiders')
-        
-        data = r.json()
-
-        return render_template('formularz.html', data = data )
-
+    	req = request.form
+    	nazwa = req['nazwa']
+    	stopien = req['stopien']
+    	klimat = req['klimat']
+    	biotop = req['biotop']
+    	tem_min = req['tem_min']
+    	tem_max = req['tem_max']
+    	lata = req['lata']
+    	r = requests.get('http://chceptasznika.herokuapp.com/spiders')
+    	data = r.json()
+    	return render_template('formularz.html', data = data, req = req, nazwa = nazwa)
     return render_template("formularz.html")
+
 
 @app.route('/ptasznik/<_id>')
 def ptasznik(_id):
+	adres = 'http://chceptasznika.herokuapp.com/spider/id/' + str(_id)
 
-    r = requests.get('http://chceptasznika.herokuapp.com/spider/id/<_id>')
-        
-    data = r.json()
+	r = requests.get(adres)
 
-    return render_template('ptasznik.html', _id = _id, data = data )
+	data = r.json()
+
+	return render_template('ptasznik.html', _id = _id, data = data )
 
 
     
