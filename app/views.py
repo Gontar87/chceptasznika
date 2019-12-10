537f33f7e2cb69b1392c0baca8243716ff07cad9
Gontar87 <34239520+Gontar87@users.noreply.github.com>
2019-12-10 13:59:07 +0100

    Dynamiczne url

--- app/views.py
+++ app/views.py
@@ -1,7 +1,12 @@
 from app import app
+from flask import Flask
 from flask import render_template
+from flask import request
+import requests
+import json
 
 
+ 
 @app.route('/') 
 def index():
     return render_template('index.html') 
@@ -10,6 +15,32 @@ def index():
 def upload():
     return render_template('upload.html')
 
-@app.route('/formularz')
+
+@app.route('/formularz', methods=["GET", "POST"])
 def formularz():
-    return render_template('formularz.html')
+    if request.method == "POST":
+        r = requests.get('http://chceptasznika.herokuapp.com/spiders')
+        
+        data = r.json()
+
+        return render_template('formularz.html', data = data )
+
+    return render_template("formularz.html")
+
+@app.route('/ptasznik/<_id>')
+def ptasznik(_id):
+
+    r = requests.get('http://chceptasznika.herokuapp.com/spider/id/<_id>')
+        
+    data = r.json()
+
+    return render_template('ptasznik.html', _id = _id, data = data )
+
+
+    
+
+
+
+
+        
+    
